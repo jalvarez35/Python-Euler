6004a46e65a468c7aeff06bd69424e4fc90746ed
Juan Alvarez <juangabo.0228@gmail.com>
2013-06-04 00:36:43 -0500

    implementation of the tests to join the factors of 3 or 5 to the 1000

--- problem1_test.py
+++ problem1_test.py
@@ -3,13 +3,8 @@ import unittest
 def es_divisible_entre_tres_o_cinco(x):
     return x%3 ==0 or x%5 == 0
 
-suma = 0
-for i in range(1,1000):
-    if es_divisible_entre_tres_o_cinco(i):
-        suma = suma + i       
-
-class Multiplos_Tres_O_Cinco(unittest.TestCase):
-
+class MultiplosTresOCinco(unittest.TestCase):
+    
     def test_prueba_que_uno_no_deberia_ser_divisible_entre_tres_o_cinco(self):
         self.assertEqual(es_divisible_entre_tres_o_cinco(1), False)
        
@@ -99,6 +94,24 @@ class Multiplos_Tres_O_Cinco(unittest.TestCase):
 
     def test_prueba_que_treinta_deberia_ser_divisible_entre_tres_o_cinco(self):
         self.assertEqual(es_divisible_entre_tres_o_cinco(30), True)
+
+def sumar_multiplos_de_tres_o_cinco_hasta(x):
+    suma = 0
+    for i in range(1, x):
+        if es_divisible_entre_tres_o_cinco(i):
+            suma = suma + i
+    return suma
+
+class SumaFactoresTresOCinco(unittest.TestCase):
+
+    def test_prueba_que_la_suma_de_los_factores_de_tres_o_cinco_menores_a_diez_deberia_ser_veintitres(self):
+        self.assertEqual(sumar_multiplos_de_tres_o_cinco_hasta(10), 23)
+
+    def test_prueba_que_la_suma_de_los_factores_de_tres_o_cinco_menores_a_cien_deberia_ser_2318(self):
+        self.assertEqual(sumar_multiplos_de_tres_o_cinco_hasta(100), 2318)
+
+    def test_prueba_que_la_suma_de_los_factores_de_tres_o_cinco_menores_a_mil_deberia_ser_233168(self):
+        self.assertEqual(sumar_multiplos_de_tres_o_cinco_hasta(1000), 233168)
 
 if __name__ == '__main__':
     unittest.main()
